a82ec62859061e694aed32fc9d6e0760d6f714c9
Ruslan.Rybin <slayervood86@gmail.com>
2024-11-04 18:12:26 +0500

    Добавил ответ на задание dockerfile

---
+++ jusan-docker/dockerfile/solution.sh
@@ -0,0 +1,4 @@
+curl -O https://stepik.org/media/attachments/lesson/686238/jusan-dockerfile.conf
+curl -O https://stepik.org/media/attachments/lesson/686238/jusan-dockerfile.zip
+sudo docker build -t nginx:jusan-dockerfile .
+sudo docker run -d -p 6060:80 --name jusan-dockerfile nginx:jusan-dockerfile
